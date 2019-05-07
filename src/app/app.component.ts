f866458da7c7f7bf8c5a7b8daf920e26a8fe300a
vadlathalaragava <vadlathalaragava@gmail.com>
2019-05-07 16:45:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,91 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { NavController, LoadingController } from 'ionic-angular';
 
-import { HomePage } from '../pages/home/home';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder';
+
+
 @Component({
-  templateUrl: 'app.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
+export class HomePage {
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  geoLatitude: number;
+  geoLongitude: number;
+  geoAccuracy:number;
+  geoAddress: string;
+ 
+  watchLocationUpdates:any; 
+  loading:any;
+  isWatching:boolean;
+ 
+  //Geocoder configuration
+  geoencoderOptions: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5
+  };
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder
+  ) {
   }
-}
+ 
+  
+    //Get current coordinates of device
+    getGeolocation(){
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.geoLatitude = resp.coords.latitude;
+        this.geoLongitude = resp.coords.longitude; 
+        this.geoAccuracy = resp.coords.accuracy; 
+        this.getGeoencoder(this.geoLatitude,this.geoLongitude);
+       }).catch((error) => {
+         alert('Error getting location'+ JSON.stringify(error));
+       });
+    }
+  
+    //geocoder method to fetch address from coordinates passed as arguments
+    getGeoencoder(latitude,longitude){
+      this.nativeGeocoder.reverseGeocode(latitude, longitude, this.geoencoderOptions)
+      .then((result: NativeGeocoderReverseResult[]) => {
+        this.geoAddress = this.generateAddress(result[0]);
+      })
+      .catch((error: any) => {
+        alert('Error getting location'+ JSON.stringify(error));
+      });
+    }
+  
+    //Return Comma saperated address
+    generateAddress(addressObj){
+        let obj = [];
+        let address = "";
+        for (let key in addressObj) {
+          obj.push(addressObj[key]);
+        }
+        obj.reverse();
+        for (let val in obj) {
+          if(obj[val].length)
+          address += obj[val]+', ';
+        }
+      return address.slice(0, -2);
+    }
+  
+  
+    //Start location update watch
+    watchLocation(){
+      this.isWatching = true;
+      this.watchLocationUpdates = this.geolocation.watchPosition();
+      this.watchLocationUpdates.subscribe((resp) => {
+        this.geoLatitude = resp.coords.latitude;
+        this.geoLongitude = resp.coords.longitude; 
+        this.getGeoencoder(this.geoLatitude,this.geoLongitude);
+      });
+    }
+  
+    //Stop location update watch
+    stopLocationWatch(){
+      this.isWatching = false;
+      this.watchLocationUpdates.unsubscribe();
+    }
 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,31 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Geolocation } from  '@ionic-native/geolocation';
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+
+ import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
     HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+    IonicModule
+   ],
   bootstrap: [IonicApp],
+  
   entryComponents: [
-    MyApp,
     HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    NativeGeocoder,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
+  
 })
 export class AppModule {}
